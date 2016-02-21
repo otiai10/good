e7e651d57382b404cf36ad25ffdd4811fc8899f7
otiai10 <otiai10@gmail.com>
2016-02-21 14:39:59 +0900

    Minimum imple

--- lib/good.ex
+++ lib/good.ex
@@ -1,2 +1,13 @@
 defmodule Good do
+
+  @doc """
+  """
+  def then({:error, reason}, _), do: {:error, reason}
+  def then({:ok, arg}, next), do: next.(arg)
+
+  @doc """
+  """
+  def otherwise({:ok, arg}, _), do: {:ok, arg}
+  def otherwise({:error, reason}, next), do: next.(reason)
+
 end
--- test/good_test.exs
+++ test/good_test.exs
@@ -2,7 +2,15 @@ defmodule GoodTest do
   use ExUnit.Case
   doctest Good
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "then" do
+    {ok?, res} = {:ok, 7}
+    |> Good.then(fn n ->
+      if rem(n, 2) == 0 , do: {:ok, n * 3}, else: {:error, "not even"}
+    end)
+    |> Good.then(fn n ->
+      {:ok, n * 2}
+    end)
+    assert ok? == :error
+    assert res == "not even"
   end
 end
