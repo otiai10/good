79d5fe51fd9abcdbf22b0461bdaad3ba6cb04c4d
otiai10 <otiai10@gmail.com>
2016-02-21 14:44:00 +0900

    otherwise

--- test/good_test.exs
+++ test/good_test.exs
@@ -13,4 +13,19 @@ defmodule GoodTest do
     assert ok? == :error
     assert res == "not even"
   end
+
+  test "otherwise" do
+    {ok?, res} = {:ok, 7}
+    |> Good.otherwise(fn n ->
+      if rem(n, 2) == 0 , do: {:ok, n * 3}, else: {:error, "not even"}
+    end)
+    |> Good.then(fn n ->
+      {:ok, n * 2}
+    end)
+    |> Good.otherwise(fn n ->
+      {:error, 3}
+    end)
+    assert ok? == :ok
+    assert res == 14
+  end
 end
