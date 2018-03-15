a6554598e852c82ef43a9615ce7bb068cf57e5cf
Hiromu OCHIAI <otiai10@gmail.com>
2018-03-15 10:37:11 +0900

    bump up version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Good.Mixfile do
 
   def project do
     [app: :good,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
