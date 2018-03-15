df4ce4a911e9d148034223cf65f0b3911b1c38bc
Hiromu OCHIAI <otiai10@gmail.com>
2018-03-15 10:36:46 +0900

    Refactor test itself

--- test/good_test.exs
+++ test/good_test.exs
@@ -1,13 +1,14 @@
 defmodule GoodTest do
   use ExUnit.Case
   doctest Good
+  import Good
 
   test "then" do
     {ok?, res} = {:ok, 7}
-    |> Good.then(fn n ->
+    |> then(fn n ->
       if rem(n, 2) == 0 , do: {:ok, n * 3}, else: {:error, "not even"}
     end)
-    |> Good.then(fn n ->
+    |> then(fn n ->
       {:ok, n * 2}
     end)
     assert ok? == :error
@@ -16,13 +17,13 @@ defmodule GoodTest do
 
   test "otherwise" do
     {ok?, res} = {:ok, 7}
-    |> Good.otherwise(fn n ->
+    |> otherwise(fn n ->
       if rem(n, 2) == 0 , do: {:ok, n * 3}, else: {:error, "not even"}
     end)
-    |> Good.then(fn n ->
+    |> then(fn n ->
       {:ok, n * 2}
     end)
-    |> Good.otherwise(fn n ->
+    |> otherwise(fn _ ->
       {:error, 3}
     end)
     assert ok? == :ok
